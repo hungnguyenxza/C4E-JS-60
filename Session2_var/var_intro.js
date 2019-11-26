9a5476857e4fd82c50ca388ff3de942e12a83e9f
Hung Nguyen Xuan <hungnx@pixelz.com>
2019-11-26 15:29:11 +0700

    session 1 & 2

---
+++ Session2_var/var_intro.js
@@ -0,0 +1,56 @@
+// alert("Xin chào"); Hiện thị ra màn hình
+
+// aaaaaaaaaaaaaaaaaaa
+// aaaaaaaaaaaaaaaaaaa
+// aaaaaaaaaaaaaaaaaaa
+// aaaaaaaaaaaaaaaaaaa
+// aaaaaaaaaaaaaaaaaaa
+
+let name;
+var age;
+const school = "HN";
+const PI = 3.14;
+name = "Hung";
+age = 20;
+
+let country = "Vietnam";
+
+// alert(name);
+// alert(age);
+// alert(country);
+
+name = "abc";
+age = 30;
+country = "My";
+
+// alert(name);
+// alert(age);
+// alert(country);
+
+// PI = 5;
+
+
+// {
+//   var a = 10;
+//   {
+//     let b = 5;
+//     var c = 20;
+//   }
+//   {
+//     let b = 10;
+//   }
+
+//   console.log(a);
+// }
+
+
+let inputName = prompt("Hãy nhập tên");
+console.log(inputName);
+
+// 1. Nhập tên, nhập tuổi
+// In ra màn hình tên, tuổi
+
+// 2. Nhập tên, nhập năm sinh
+// In ra màn hình tên, tuổi
+
+
